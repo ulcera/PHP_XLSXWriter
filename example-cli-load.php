15e617af671cf1874cf789fa5e6cd0054e047690
Lorenzo Nesello <toyo86@gmail.com>
2015-01-27 17:27:51 +0100

    Update example-cli-load.php

--- example-cli-load.php
+++ example-cli-load.php
@@ -2,7 +2,8 @@
 include_once("xlsxwriter.class.php");
 
 $writer = new XLSXWriter();
-$writer->writeSheetHeader('Sheet1', array('c1'=>'string','c2'=>'string','c3'=>'string','c4'=>'string') );//optional
+$arr_lenght = array(150,16,5,50);//array with lenght of column
+$writer->writeSheetHeader('Sheet1', array('c1'=>'string','c2'=>'string','c3'=>'string','c4'=>'string'),$arr_lenght );//optional
 for($i=0; $i<50000; $i++)
 {
     $writer->writeSheetRow('Sheet1', array(rand()%10000,rand()%10000,rand()%10000,rand()%10000) );
